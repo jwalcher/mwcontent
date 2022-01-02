b6a3997441bdb601885fb265075576af99aa31f5
jwalcher <walcher@uni-heidelberg.de>
2022-01-02 10:38:09 +0200

    us heroku db during development

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+
+const parse = require('pg-connection-string').parse;
+
+
+// const config = parse(dbUrl);
+// DATABASE_URL is set in .env to whatever database heroku assigned to us
+// when that changes, execute
+// heroku config:get DATABASE_URL -a mwcontent
+// and copy result into .env
+// (at the moment node.js does not support "command substitution" in dotenv)
+
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  });
